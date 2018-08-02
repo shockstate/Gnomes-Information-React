34bcdde20345458a4c78a3d5ab7c189d87aee2f9
Guillem Bonafonte Pardas <guillem.bonafonte@raona.com>
2018-08-02 09:36:01 +0200

    hello world

---
+++ src/components/Game.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../index.css';
+
+export class Game extends React.Component {
+    render(){
+        return(
+            <h1> Hello world! </h1>
+        )
+    }
+}
